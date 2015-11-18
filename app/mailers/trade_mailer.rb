ef17c2f98e6ff1d84f26b4f53170e73cf7c696d8
Justin Armstrong <pseudopeach@gmail.com>
2015-11-18 09:20:06 -0800

    sets up mailers

---
+++ app/mailers/trade_mailer.rb
@@ -0,0 +1,9 @@
+class TradeMailer < ApplicationMailer
+  def execution_notification(trade, user)
+    @user = user
+    @trade = trade
+    @share_count = @user.reserve.where(resource_id: trade.candidate_id).first.qty
+    @url  = "http://gop-trade.herokuapp.com?user_key=#{user.key}"
+    mail(to: @user.email, subject: 'Trade Executed')
+  end
+end
